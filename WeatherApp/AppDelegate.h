8af11219488731ce87e17c7d1ffe4436cf7c53c0
Pete Holdsworth <Pete@petes-air>
2018-03-20 19:18:36 +0000

    Initial Commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by Pete Holdsworth on 20/03/2018.
+//  Copyright © 2018 Onyx Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherApp
+//
+//  Created by Pete Holdsworth on 20/03/2018.
+//  Copyright © 2018 Onyx Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
